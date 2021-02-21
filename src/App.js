4199378fa92778e199dde785859429cb6484c0a3
iamshin <xinlong95@hotmail.com>
2021-02-21 22:47:09 +0800

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import {JumbotronContainer} from './containers/jumbotron';
-import {FooterContainer} from './containers/footer'
-import { FaqsContainer } from './containers/faq';
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import * as ROUTES from './constants/routes'
+import {Home,Browse, Signin,Signup} from './pages';
 
 export default function App(){
   return (
-    <>
-      <JumbotronContainer />;
-      <FaqsContainer/>
-      <FooterContainer/>
-    </>
+    <Router>
+    <Route exact path="/browse">
+        <Browse/>
+      </Route>
+      <Route exact path="/signin">
+        <Signin/>
+      </Route>
+      <Route exact path="/signup">
+        <Signup/>
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home/>
+      </Route>
+      <Route exact path = "/user">
+        <p>Hello I am a user</p>
+      </Route>
+    </Router>
   )
 }
