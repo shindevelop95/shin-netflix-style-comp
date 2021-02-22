f631e92682fc576935fc1b425d95b6157e36e94b
iamshin <xinlong95@hotmail.com>
2021-02-22 23:21:53 +0800

    Added Header

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {JumbotronContainer} from '../containers/jumbotron';
 import {FooterContainer} from '../containers/footer'
 import { FaqsContainer } from '../containers/faq';
+import {HeaderContainer} from '../containers/header';
 
 export default function Home(){
     return(
-        <>
+       
+        <HeaderContainer>
             <JumbotronContainer />;
-            <FaqsContainer/>
-            <FooterContainer/>
-        </>
+                <FaqsContainer/>
+                <FooterContainer/>
+        </HeaderContainer>
+            
+   
     )
 }
