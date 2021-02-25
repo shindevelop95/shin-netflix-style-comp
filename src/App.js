8d6d3e8bf5bc3c13043fcdd8686838b19c093c23
iamshin <xinlong95@hotmail.com>
2021-02-25 22:23:34 +0800

    Added custom hook

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
 import * as ROUTES from './constants/routes'
 import { IsUserRedirect,ProtectedRoute } from './helpers/routes';
 import {Home,Browse, Signin,Signup} from './pages';
+import {useAuthListener} from './hooks'
 
 export default function App(){
-  const user = null;
+  const user = useAuthListener();
   return (
     <Router>
      <IsUserRedirect
