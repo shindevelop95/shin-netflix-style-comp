7391128f50db79998aba341668dd940dc3046524
iamshin <xinlong95@hotmail.com>
2021-02-25 20:50:21 +0800

    Added signup ui

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,63 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import { FooterContainer } from '../containers/footer';
+import {HeaderContainer} from '../containers/header';
+import {FirebaseContext} from '../context/firebase'
+import * as ROUTES from '../constants/routes';
+import {useHistory} from 'react-router-dom'
+import {Form} from '../components';
+
 
 export default function Signup(){
-    return (<p>Hello frmom the signup</p>)
+    const history = useHistory();
+    const {firebase} = useContext(FirebaseContext);
+    const [firstName,setFirstName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [ password, setPassword] = useState('');
+    const [error,setError] = useState('');
+    const isInvalid = password =='' || emailAddress =='';
+
+    const handleSignup = (event) => {
+        event.preventDefault();
+    }
+    return (
+        <>
+        <HeaderContainer>
+            <Form>
+                <Form.Title>Sign Up</Form.Title>
+                {error && <Form.Error>{error}</Form.Error>}
+                <Form.Base onSubmit={handleSignup} method="POST">
+                    <Form.Input
+                        placeholder="First name"
+                        value={firstName}
+                        onChange={((e) => setFirstName(e.target.value))}
+                    />
+                     <Form.Input
+                        placeholder="Email address"
+                        value={emailAddress}
+                        onChange={((e) => setEmailAddress(e.target.value))}
+                    />
+                    <Form.Input
+                        type="password"
+                        value={password}
+                        autoComplete="off"
+                        placeholder="Password"
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+                    <Form.Submit disabled={isInvalid} type="submit">
+                        Sign Up
+                    </Form.Submit>
+                    
+                    <Form.Text>
+                        Already a user? <Form.Link to="/signin">Sign in now</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+                    </Form.TextSmall>
+
+                </Form.Base>
+            </Form>
+        </HeaderContainer>
+        <FooterContainer/>
+        </>
+    )
 }
