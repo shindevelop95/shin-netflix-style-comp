2446ae263afd5e97fb5d1ab258a6ca9871d3a487
iamshin <xinlong95@hotmail.com>
2021-02-25 22:50:54 +0800

    Added utils

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {useContent} from '../hooks'
+import selectionFilter from '../utils/selection-filter'
 
 export default function Browse(){
     const {series} = useContent('series');
+    const {films} = useContent('films');
+    const slides = selectionFilter ({series,films});
+    console.log(slides);
     return (<p>Hello frmom the browse</p>)
 }
