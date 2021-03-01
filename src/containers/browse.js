0dc2cf4004a6fc8daccff480435a6a9744fcc1ed
iamshin <xinlong95@hotmail.com>
2021-03-01 22:33:52 +0800

    Added Spinner

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import { SelectProfileContainer } from './profiles'
+import {FirebaseContext} from '../context/firebase'
+import {Loading} from '../components';
 
 export function BrowseContainer({slides}){
-    return (<SelectProfileContainer/>)}
+    const [profile,setProfile] = useState({});
+    const {firebase} = useContext(FirebaseContext);
+    const [loading,setLoading] = useState(true);
+    const user = firebase.auth().currentUser || {};
+
+    useEffect(() => {
+        console.log('profile',profile);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000)
+    }, [profile.displayName])
+    return profile.displayName ? (
+        loading ? (
+            <Loading src={user.photoURL}/>
+        ):null):
+        (<SelectProfileContainer user={user} setProfile={setProfile}/>)}
