9e2d780913e2871c8ccbbdd512596f354050d614
iamshin <xinlong95@hotmail.com>
2021-03-02 18:55:55 +0800

    Movie Cards

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -73,7 +73,30 @@ export function BrowseContainer({slides}){
         </Header>
 
         <Card.Group>
-
+            {slideRows.map((slideItem) => (
+                <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                    <Card.Title>{slideItem.title}</Card.Title>
+                    <Card.Entities>
+                        {slideItem.data.map((item) => (
+                            <Card.Item key={item.docId} item={item}>
+                                <Card.Image
+                                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                                />
+                                <Card.Meta>
+                                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                                    <Card.Text>{item.description}</Card.Text>
+                                </Card.Meta>
+                            </Card.Item>
+                        ))}
+                    </Card.Entities>
+                   {/* <Card.Feature category={category}>
+                        <Player>
+                            <Player.Button/>
+                            <Player.Video src="videos/bunny.mp4"/>
+                        </Player>
+                    </Card.Feature>*/}
+                </Card>
+            ))}
         </Card.Group>
         </>):
         (<SelectProfileContainer user={user} setProfile={setProfile}/>)}
