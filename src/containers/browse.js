97bf2ec5a9a5601217ce815a88564b4b68695651
iamshin <xinlong95@hotmail.com>
2021-03-11 22:57:46 +0800

    added live search

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -4,6 +4,7 @@ import {FooterContainer} from './footer'
 import {FirebaseContext} from '../context/firebase'
 import {Card,Header,Loading, Player} from '../components';
 import * as ROUTES from '../constants/routes';
+import Fuse from 'fuse.js';
 import logo from '../logo.svg';
 
 
@@ -26,6 +27,19 @@ export function BrowseContainer({slides}){
     useEffect(() => {
         setSlideRows(slides[category]);
     },[slides,category]);
+
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, {
+            keys: ['data.description', 'data.title', 'data.genre'],
+        })
+        const results = fuse.search(searchTerm).map(({item}) => item);
+
+        if(slideRows.length > 0 && searchTerm.length > 3 && results.length > 0){
+            setSlideRows(results);
+        }else{
+            setSlideRows(slides[category]);
+        }
+    },[searchTerm])
     
     return profile.displayName ? (
         
